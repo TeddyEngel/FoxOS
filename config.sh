f997151ec1b759191bad053917f9033b38eabad1
Teddy Engel <engel.teddy@gmail.com>
2017-01-31 20:10:00 +0000

    - Add CPP compiler to config.sh

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ export HOST=${HOST:-$(./default-host.sh)}
 export AR=${HOST}-ar
 export AS=${HOST}-as
 export CC=${HOST}-gcc
-export CXX=${HOST}-g++
+export CPP=${HOST}-g++
 
 export PREFIX=/usr
 export EXEC_PREFIX=$PREFIX
@@ -24,9 +24,11 @@ export OSNAME="$(./osname.sh)"
 # Configure the cross-compiler to use the desired system root.
 export SYSROOT="$(pwd)/sysroot"
 export CC="$CC --sysroot=$SYSROOT"
+export CPP="$CPP --sysroot=$SYSROOT"
 
 # Work around that the -elf gcc targets doesn't have a system include directory
 # because it was configured with --without-headers rather than --with-sysroot.
 if echo "$HOST" | grep -Eq -- '-elf($|-)'; then
   export CC="$CC -isystem=$INCLUDEDIR"
+  export CP="$CP -isystem=$INCLUDEDIR"
 fi
