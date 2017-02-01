036fb9d16a717d5106a251083484a153ecb7ffce
Teddy Engel <engel.teddy@gmail.com>
2017-02-01 21:14:06 +0000

    - Almost nothing

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -10,5 +10,5 @@ void kernel_main(void)
     const char* FOX_BANNER = " ,-.      .-,\n |-.\\ __ /.-|\n \\  `    `  /\n / _     _  \\\n | _`q  p _ |\n \'._=/  \\=_.'\n   {`\\()/`}`\\                   FoxOS\n   {      }  \\\n   |{    }    \\\n   \\ '--'   .- \\\n   |-      /    \\\n   | | | | |     ;\n   | | |.;.,..__ |\n .-\"\";`         `|\n /    |           /\n `-../____,..---'`\n ";
 
 	terminal_initialize();
-	printf(FOX_BANNER);
+    printf(FOX_BANNER);
 }
