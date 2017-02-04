18551a1c1050dfdceb14d059ca86367e5dc6157d
Teddy Engel <engel.teddy@gmail.com>
2017-02-04 16:17:30 +0000

    - Bit of cleanup on the gdt + Adding some todos

--- kernel/arch/i386/gdt.c
+++ kernel/arch/i386/gdt.c
@@ -1,7 +1,7 @@
 #include <kernel/gdt.h>
 
-/* Our GDT, with 3 entries, and finally our special GDT pointer */
-gdt_entry_t gdt[3];
+// Our GDT entries: code and data for kernel, code and data for user mode, null entry
+gdt_entry_t gdt_entries[5];
 gdt_ptr_t gp;
 
 /* Setup a descriptor in the Global Descriptor Table */
@@ -9,17 +9,17 @@ void gdt_set_gate(int num, unsigned long base, unsigned long limit,
                            unsigned char access, unsigned char gran)
 {
     /* Setup the descriptor base address */
-    gdt[num].base_low = (base & 0xFFFF);
-    gdt[num].base_middle = (base >> 16) & 0xFF;
-    gdt[num].base_high = (base >> 24) & 0xFF;
+    gdt_entries[num].base_low = (base & 0xFFFF);
+    gdt_entries[num].base_middle = (base >> 16) & 0xFF;
+    gdt_entries[num].base_high = (base >> 24) & 0xFF;
 
     /* Setup the descriptor limits */
-    gdt[num].limit_low = (limit & 0xFFFF);
-    gdt[num].granularity = ((limit >> 16) & 0x0F);
+    gdt_entries[num].limit_low = (limit & 0xFFFF);
+    gdt_entries[num].granularity = ((limit >> 16) & 0x0F);
 
     /* Finally, set up the granularity and access flags */
-    gdt[num].granularity |= (gran & 0xF0);
-    gdt[num].access = access;
+    gdt_entries[num].granularity |= (gran & 0xF0);
+    gdt_entries[num].access = access;
 }
 
 /* Should be called by main. This will setup the special GDT
@@ -29,11 +29,11 @@ void gdt_set_gate(int num, unsigned long base, unsigned long limit,
 *  new segment registers */
 void gdt_install()
 {
-    /* Setup the GDT pointer and limit */
-    gp.limit = (sizeof(gdt_entry_t) * 3) - 1;
-    gp.base = (unsigned int)&gdt;
+    // Setup the GDT pointer and limit
+    gp.limit = (sizeof(gdt_entry_t) * 5) - 1;
+    gp.base = (unsigned int)&gdt_entries;
 
-    /* Our NULL descriptor */
+    // Our NULL descriptor
     gdt_set_gate(0, 0, 0, 0, 0);
 
     /* The second entry is our Code Segment. The base address
@@ -47,6 +47,12 @@ void gdt_install()
     *  same as our code segment, but the descriptor type in
     *  this entry's access byte says it's a Data Segment */
     gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
+
+    // User mode code segment
+    gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF);
+
+    // User mode data segment 
+    gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF);
 
     /* Flush out the old GDT and install the new changes! */
     gdt_flush((unsigned int)&gp);
--- kernel/include/kernel/gdt.h
+++ kernel/include/kernel/gdt.h
@@ -8,12 +8,12 @@
 *  compiler "optimization" by packing */
 struct gdt_entry
 {
-    unsigned short limit_low;
-    unsigned short base_low;
-    unsigned char base_middle;
-    unsigned char access;
-    unsigned char granularity;
-    unsigned char base_high;
+    unsigned short limit_low; // TODO: u16int
+    unsigned short base_low; // TODO: u16int
+    unsigned char base_middle; // TODO: u8int
+    unsigned char access; // TODO: u8int
+    unsigned char granularity; // TODO: u8int
+    unsigned char base_high; // TODO: u8int
 } __attribute__((packed));
 typedef struct gdt_entry gdt_entry_t;
 
@@ -21,8 +21,8 @@ typedef struct gdt_entry gdt_entry_t;
 *  taken up by the GDT, minus 1. Again, this NEEDS to be packed */
 struct gdt_ptr
 {
-    unsigned short limit; // TODO: Replace by u16int
-    unsigned int base; // TODO: Replace by u32int
+    unsigned short limit; // TODO: u16int
+    unsigned int base; // TODO: u32int
 } __attribute__((packed));
 typedef struct gdt_ptr gdt_ptr_t;
 
@@ -33,7 +33,7 @@ extern "C"
 #else
 extern
 #endif
-void gdt_flush(unsigned int); // TODO: Replace by u32int
+void gdt_flush(unsigned int); // TODO: u32int
 
 #if defined(__cplusplus)
 extern "C"
