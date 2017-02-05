18e84e8566eb71b9ece42d037bde9e5bdb3d562b
Teddy Engel <engel.teddy@gmail.com>
2017-02-05 20:30:43 +0000

    Add debug interrupts in kernel_main

--- kernel/kernel/kernel.cpp
+++ kernel/kernel/kernel.cpp
@@ -16,9 +16,11 @@ void kernel_early()
 extern "C"
 void kernel_main(void)
 {
-    const char* FOX_BANNER = " ,-.      .-,\n |-.\\ __ /.-|\n \\  `    `  /\n / _     _  \\\n | _`q  p _ |\n \'._=/  \\=_.'\n   {`\\()/`}`\\                   FoxOS\n   {      }  \\\n   |{    }    \\\n   \\ '--'   .- \\\n   |-      /    \\\n   | | | | |     ;\n   | | |.;.,..__ |\n .-\"\";`         `|\n /    |           /\n `-../____,..---'`\n ";
+    const char* FOX_BANNER = " ,-.      .-,\n |-.\\ __ /.-|\n \\  `    `  /\n / _     _  \\\n | _`q  p _ |\n \'._=/  \\=_.'\n   {`\\()/`}`\\                   FoxOS\n   {      }  \\\n   |{    }    \\\n   \\ '--'   .- \\\n   |-      /    \\\n   | | | | |     ;\n   | | |.;.,..__ |\n .-\"\";`         `|\n /    |           /\n `-../____,..---'`\n";
 	
     printf(FOX_BANNER);
+    
+    asm volatile ("int $0x0");
     asm volatile ("int $0x3");
-    // asm volatile ("int $0x4"); 
+    asm volatile ("int $0x4"); 
 }
