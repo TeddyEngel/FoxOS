7424c9bfa1b46317537555a501e35df9e5f8903b
Teddy Engel <engel.teddy@gmail.com>
2017-02-05 17:34:30 +0000

    Add proper typedefs to gdt / idt

--- kernel/include/kernel/gdt.h
+++ kernel/include/kernel/gdt.h
@@ -6,7 +6,7 @@
 /* Defines a GDT entry. We say packed, because it prevents the
 *  compiler from doing things that it thinks is best: Prevent
 *  compiler "optimization" by packing */
-struct gdt_entry
+typedef struct gdt_entry
 {
     uint16_t limit_low;
     uint16_t base_low;
@@ -14,17 +14,15 @@ struct gdt_entry
     uint8_t access;
     uint8_t granularity;
     uint8_t base_high;
-} __attribute__((packed));
-typedef struct gdt_entry gdt_entry_t;
+} __attribute__((packed)) gdt_entry_t;
 
 /* Special pointer which includes the limit: The max bytes
 *  taken up by the GDT, minus 1. Again, this NEEDS to be packed */
-struct gdt_ptr
+typedef struct gdt_ptr
 {
     uint16_t limit;
     uint32_t base;
-} __attribute__((packed));
-typedef struct gdt_ptr gdt_ptr_t;
+} __attribute__((packed)) gdt_ptr_t;
 
 class gdt_manager
 {
