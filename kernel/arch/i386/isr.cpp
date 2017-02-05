f008b288c7a7419013bdbacca9c5f88c9733915b
Teddy Engel <engel.teddy@gmail.com>
2017-02-05 20:30:25 +0000

    Alter interrupt handler debug message

--- kernel/arch/i386/isr.cpp
+++ kernel/arch/i386/isr.cpp
@@ -2,9 +2,7 @@
 
 #include <cstdio>
 
-// This gets called from our ASM interrupt handler stub.
 void isr_handler(registers_t regs)
 {
-    printf("recieved interrupt\n");
-    // printf("recieved interrupt: %d\n", regs.int_no);
+    printf("Interrupt: %d %d\n", regs.int_no, regs.err_code);
 }
