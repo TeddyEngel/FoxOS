51d986e9e3a0fa4dc61db327219f08e81396fe39
Teddy Engel <engel.teddy@gmail.com>
2017-02-11 18:59:39 +0000

    Add memory manager header / impl

---
+++ kernel/arch/i386/memoryManager.cpp
@@ -0,0 +1,14 @@
+#include <kernel/memoryManager.h>
+
+const size_t tty_manager::VGA_WIDTH = 80;
+
+uint16_t* tty_manager::_buffer;
+
+void memoryManager::initialize(void)
+{
+    // _cursor_row = 0;
+    // _cursor_column = 0;
+    // _color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+    // _buffer = VGA_MEMORY;
+    // clear();
+}
---
+++ kernel/include/kernel/memoryManager.h
@@ -0,0 +1,13 @@
+#ifndef _KERNEL_MEMORY_MANAGER_H
+#define _KERNEL_MEMORY_MANAGER_H
+
+// #include <cstddef>
+#include <cstdint>
+
+class memoryManager
+{
+public:
+    static void initialize();
+};
+
+#endif /* _KERNEL_MEMORY_MANAGER_H */
