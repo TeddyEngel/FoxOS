f3f1fdf3cc05ce7ebc055de2164cb6b8ef9e74a0
Teddy Engel <engel.teddy@gmail.com>
2017-02-12 13:58:07 +0000

    Extract pit types in their own header

--- kernel/include/kernel/timer.h
+++ kernel/include/kernel/timer.h
@@ -9,13 +9,9 @@
 
 #include <kernel/registers.h>
 
-#define DEFAULT_FREQUENCY 50
+#include <kernel/pit_types.h>
 
-#define PIT_DATA_PORT_CHANNEL_0 0x40 // Output connected to IRQ0
-#define PIT_DATA_PORT_CHANNEL_1 0x41 // Controls refresh rates of DRAM
-#define PIT_DATA_PORT_CHANNEL_2 0x42 // Controls the pc speaker
-#define PIT_COMMAND_PORT 0x43
-#define PIT_REPEATING_MODE 0x36 // To set the clock to repeat once it reaches 0
+#define DEFAULT_FREQUENCY 50
 
 class timer_manager
 {
