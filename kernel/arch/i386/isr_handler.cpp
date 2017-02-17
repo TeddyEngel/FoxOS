c79d09fa42355d65aa741112f2f7302719fcab9c
Teddy Engel <engel.teddy@gmail.com>
2017-02-17 22:17:18 +0000

    Handle interrupts in isr_handler as well using the interrupt_manager

--- kernel/arch/i386/isr_handler.cpp
+++ kernel/arch/i386/isr_handler.cpp
@@ -2,7 +2,19 @@
 
 #include <cstdio>
 
+#include <kernel/KernelManager.h>
+#include <kernel/InterruptManager.h>
+
+extern KernelManager kernelManager;
+
 void isr_handler(registers_t regs)
 {
-    printf("Interrupt: %d %d\n", regs.int_no, regs.err_code);
+    InterruptManager& interruptManager = kernelManager.getInterruptManager();
+    if (interruptManager.hasHandler(regs.int_no))
+    {
+        fct_handler handler = interruptManager.getHandler(regs.int_no);
+        handler(regs);
+    }
+    else
+        printf("Unhandled interrupt: %d (%d)\n", regs.int_no, regs.err_code);
 }
