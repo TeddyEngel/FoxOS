cf0a4a5edaf331505e7dcd007ce4a5ab78f63baf
Teddy Engel <engel.teddy@gmail.com>
2017-02-19 19:06:17 +0000

    Add stub for ordered_array and add to makefile

---
+++ libc++/collection/ordered_array.cpp
@@ -0,0 +1,33 @@
+#include <ordered_array>
+
+int8_t standard_lessthan_predicate(type_t a, type_t b)
+{
+  return 0;
+}
+
+ordered_array::ordered_array(uint32_t maxSize, lessthan_predicate_t lessThan)
+{
+}
+
+ordered_array::ordered_array(void *addr, uint32_t maxSize, lessthan_predicate_t lessThan)
+{
+}
+
+ordered_array::~ordered_array()
+{
+}
+
+void ordered_array::insert(type_t item)
+{
+
+}
+
+type_t ordered_array::lookup(uint32_t i)
+{
+  return 0;
+}
+
+void ordered_array::erase(uint32_t i)
+{
+
+}
