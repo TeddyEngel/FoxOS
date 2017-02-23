dfb58db27fb24f75d2b92fe0f95e74bffad9b320
Teddy Engel <engel.teddy@gmail.com>
2017-02-23 17:28:37 +0000

    Load the GDT pointer within C++ instead

--- kernel/arch/i386/gdt_flush.asm
+++ kernel/arch/i386/gdt_flush.asm
@@ -1,9 +1,6 @@
 [GLOBAL gdt_flush]    ; Allows the C code to call gdt_flush().
 
 gdt_flush:
-   ;mov eax, [esp+4]  ; Get the pointer to the GDT, passed as a parameter.
-   ;lgdt [eax]        ; Load the new GDT pointer
-
    mov ax, 0x10      ; 0x10 is the offset in the GDT to our data segment
    mov ds, ax        ; Load all data segment selectors
    mov es, ax
