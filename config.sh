2263bbb92c96bc747f7077d6e65d628d6d68d422
Teddy Engel <engel.teddy@gmail.com>
2017-02-25 16:30:17 +0000

    Add no red zone flag

--- config.sh
+++ config.sh
@@ -17,7 +17,7 @@ export INCLUDEDIR=$PREFIX/include
 
 export AFLAGS=''
 export CFLAGS='-O2 -g'
-export CPPFLAGS='-O2 -g -fno-stack-protector -fno-use-cxa-atexit'
+export CPPFLAGS='-O2 -g -fno-stack-protector -fno-use-cxa-atexit -mno-red-zone'
 
 # Configure the target os name
 export OSNAME="$(./osname.sh)"
