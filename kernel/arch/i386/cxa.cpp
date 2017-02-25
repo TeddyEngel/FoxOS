44de82a2b9de83cfd4a1bcd30e6838a63dcb73fa
Teddy Engel <engel.teddy@gmail.com>
2017-02-25 14:29:22 +0000

    Add stubs for cxaexit etc in cxa - not used for now

--- kernel/arch/i386/cxa.cpp
+++ kernel/arch/i386/cxa.cpp
@@ -1,6 +1,18 @@
 #include <kernel/cxa.h>
 
+// void *__dso_handle;
+
 void __cxa_pure_virtual()
 {
     // Do nothing or print an error message.
 }
+
+// int __cxa_atexit(void (*destructor) (void *), void *arg, void *dso)
+// {
+//     return 0;
+// }
+
+// void __cxa_finalize(void *f)
+// {
+
+// }
--- kernel/include/kernel/cxa.h
+++ kernel/include/kernel/cxa.h
@@ -4,6 +4,8 @@
 extern "C"
 {
     void __cxa_pure_virtual();
+    // int __cxa_atexit(void (*destructor) (void *), void *arg, void *dso);
+    // void __cxa_finalize(void *f);
 }
 
 #endif /* _KERNEL_CXA */
