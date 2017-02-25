92a4a1d602e26e617840c1202ba8c3ec2f3a1bf4
Teddy Engel <engel.teddy@gmail.com>
2017-02-25 13:03:32 +0000

    Add cxa for pure virtual calls in kernel

---
+++ kernel/arch/i386/cxa.cpp
@@ -0,0 +1,6 @@
+#include <kernel/cxa.h>
+
+void __cxa_pure_virtual()
+{
+    // Do nothing or print an error message.
+}
---
+++ kernel/include/kernel/cxa.h
@@ -0,0 +1,9 @@
+#ifndef _KERNEL_CXA
+#define _KERNEL_CXA 1
+
+extern "C"
+{
+    void __cxa_pure_virtual();
+}
+
+#endif /* _KERNEL_CXA */
