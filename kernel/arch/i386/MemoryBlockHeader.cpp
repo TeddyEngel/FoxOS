3db6bac623ff2d7c445e9e611f4e2d4d37bdfbf2
Teddy Engel <engel.teddy@gmail.com>
2017-03-05 16:27:14 +0000

    Add less_than predicate specific for a memory block header

---
+++ kernel/arch/i386/MemoryBlockHeader.cpp
@@ -0,0 +1,6 @@
+#include <kernel/MemoryBlockHeader.h>
+
+static int8_t MemoryBlockHeader::less_than(void* a, void* b)
+{
+   return (((MemoryBlockHeader*)a)->_size < ((MemoryBlockHeader*)b)->_size) ? 1 : 0;
+}
--- kernel/include/kernel/MemoryBlockHeader.h
+++ kernel/include/kernel/MemoryBlockHeader.h
@@ -4,11 +4,14 @@
 #include <cstdint>
 
 class MemoryBlockHeader
-{    
+{
 public:
-  uint32_t _magic;  // Magic number, used for error checking and identification.
-  uint8_t _isHole; // 1 if this is a hole, 0 if this is a block.
-  uint32_t _size;   // Size of the block, including this and the footer.
+    static int8_t less_than(void* a, void* b);
+
+public:
+    uint32_t _magic;  // Magic number, used for error checking and identification.
+    uint8_t _isHole; // 1 if this is a hole, 0 if this is a block.
+    uint32_t _size;   // Size of the block, including this and the footer.
 };
 
 #endif /* _KERNEL_MEMORY_BLOCK_HEADER_H */
